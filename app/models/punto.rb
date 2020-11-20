17c52d928c27cda4244cab00d21308b81d1b0466
cassiano yasumitsu <cassiano@usuarioincomum.com>
2020-11-20 21:29:21 +0900

    period

--- app/models/punto.rb
+++ app/models/punto.rb
@@ -1,4 +1,8 @@
 class Punto < ApplicationRecord
    belongs_to :user
    belongs_to :genba
+
+   PERIOD = %w(Day Night DayOff Morning Afternoon)
+
+   validates :period, inclusion: { in: PERIOD }
 end
