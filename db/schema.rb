17878bc66cc9a47a0e16444b68e5e873e11d171f
cassiano yasumitsu <cassiano@usuarioincomum.com>
2020-11-23 21:01:15 +0900

    pdf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_115200) do
+ActiveRecord::Schema.define(version: 2020_11_20_133911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_09_06_115200) do
     t.bigint "genba_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "period"
     t.index ["genba_id"], name: "index_puntos_on_genba_id"
     t.index ["user_id"], name: "index_puntos_on_user_id"
   end
