fd6d1ee151579efefa66aa7e93cb15aaf8195adc
cassiano yasumitsu <cassiano@usuarioincomum.com>
2020-11-29 22:02:27 +0900

    pwa

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,18 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+{
+  "background_color": "white",
+  "description": "Best people management for your business. ",
+  "display": "fullscreen",
+  "name": "Yudensetsu App",
+  "icons": [
+    {
+      "src": "/android-icon-36x36.png",
+      "sizes": "36x36",
+      "type": "image/png",
+      "density": "0.75"
+    },
+    {},
+    {}
+  ]
+}
