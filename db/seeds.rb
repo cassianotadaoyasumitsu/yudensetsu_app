c84685a18d38b70b678314b80f47b2611d68f944
cassiano yasumitsu <cassiano@usuarioincomum.com>
2020-12-15 14:17:45 +0900

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 require 'faker'
 
 User.destroy_all
-Company.destroy_all
+# Company.destroy_all
 
 
 puts 'Creating Admin Master'
@@ -30,38 +30,38 @@ admin.save
 
 puts 'Admin Created'
 
-puts 'Creating Users'
-10.times do
-  params = {}
-  params[:name] = Faker::Name.name
-  params[:furigana] = 'フリガナ'
-  params[:address] = Faker::Address.full_address
-  params[:pay_number] = '001'
-  params[:phone] = '07040670084'
-  params[:email] = Faker::Internet.email
-  params[:role] = '電気技師'
-  params[:level_employee] = rand(1..5)
-  params[:started_date] = Faker::Date.in_date_period
-  params[:password] = '123456'
-  params[:admin] = 'false'
-  user = User.new(params)
-  user.save
-end
-puts 'Users Created'
-puts 'Creating Companies'
-10.times do
-  params = {}
-  params[:name] = Faker::Company.name
-  params[:furigana] = 'フリガナ'
-  params[:ceo] = Faker::Name.name
-  params[:address] = Faker::Address.full_address
-  params[:phone] = '00000000000'
-  params[:fax] = '0000000000'
-  params[:contact] = Faker::Name.name
-  params[:email] = Faker::Internet.email
-  params[:closure] = rand(1..31)
-  company = Company.new(params)
-  company.save
+# puts 'Creating Users'
+# 10.times do
+#   params = {}
+#   params[:name] = Faker::Name.name
+#   params[:furigana] = 'フリガナ'
+#   params[:address] = Faker::Address.full_address
+#   params[:pay_number] = '001'
+#   params[:phone] = '07040670084'
+#   params[:email] = Faker::Internet.email
+#   params[:role] = '電気技師'
+#   params[:level_employee] = rand(1..5)
+#   params[:started_date] = Faker::Date.in_date_period
+#   params[:password] = '123456'
+#   params[:admin] = 'false'
+#   user = User.new(params)
+#   user.save
+# end
+# puts 'Users Created'
+# puts 'Creating Companies'
+# 10.times do
+#   params = {}
+#   params[:name] = Faker::Company.name
+#   params[:furigana] = 'フリガナ'
+#   params[:ceo] = Faker::Name.name
+#   params[:address] = Faker::Address.full_address
+#   params[:phone] = '00000000000'
+#   params[:fax] = '0000000000'
+#   params[:contact] = Faker::Name.name
+#   params[:email] = Faker::Internet.email
+#   params[:closure] = rand(1..31)
+#   company = Company.new(params)
+#   company.save
 
-end
-puts 'Companies Created'
+# end
+# puts 'Companies Created'
