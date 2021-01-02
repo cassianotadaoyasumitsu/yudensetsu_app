53421101e120cd5d6e039d93dc161361f16b6575
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-01-02 10:58:46 +0900

    car path

--- app/controllers/carpuntos_controller.rb
+++ app/controllers/carpuntos_controller.rb
@@ -29,6 +29,6 @@ class CarpuntosController < ApplicationController
   end
 
   def carpunto_params
-    params.require(:carpunto).permit(:start, :end, :oil, :oiltime, :fuel, :fueltime)
+    params.require(:carpunto).permit(:start, :end, :oil, :oiltime, :fuel, :fueltime, :car_id)
   end
 end
